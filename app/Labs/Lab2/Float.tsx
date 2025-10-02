f4e0108478a68d32a57388d90ef560e810df1aeb
Amy Eng <amyeng895@gmail.com>
2025-10-01 20:53:45 -0400

    fixing lab 2 deployment issues

--- app/Labs/Lab2/Float.tsx
+++ app/Labs/Lab2/Float.tsx
@@ -14,7 +14,8 @@ export default function Float() {
                           wd-bg-color-red">
             Red </div>
           <img className="wd-float-right"
-            src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+            src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+            alt="Starship rocket"/>
           <div className="wd-float-done"></div>
         </div>
       </div>

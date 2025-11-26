0df8d2f759cf5c398747e11439311de1acb074c1
Amy Eng <amyeng895@gmail.com>
2025-11-26 16:25:59 -0500

    a5 up to 5.3.3.1

--- app/(Kambaz)/Account/client.ts
+++ app/(Kambaz)/Account/client.ts
@@ -1,26 +1,36 @@
 import axios from "axios";
-export const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
-// Use relative paths for Next.js API routes
-export const USERS_API = `/api/users`;
+
+export const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER || "http://localhost:4000";
+export const USERS_API = `${HTTP_SERVER}/api/users`;
+
+export const axiosWithCredentials = axios.create({
+  baseURL: HTTP_SERVER,
+  withCredentials: true, 
+});
+console.log("HTTP_SERVER =", HTTP_SERVER);
+console.log("USERS_API =", USERS_API);
 
 export const signin = async (credentials: any) => {
-  const response = await axios.post( `${USERS_API}/signin`, credentials );
-  return response.data;
-};
-
-export const signup = async (user: any) => {
-    const response = await axios.post(`${USERS_API}/signup`, user);
-    return response.data;
-  };
-  
-export const updateUser = async (user: any) => {
-  const response = await axios.put(`${USERS_API}/${user._id}`, user);
+  const response = await axiosWithCredentials.post(`${USERS_API}/signin`, credentials);
   return response.data;
 };
 
 export const profile = async () => {
-  const response = await axios.post(`${USERS_API}/profile`);
+  const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
   return response.data;
 };
 
-  
+export const signup = async (user: any) => {
+  const response = await axiosWithCredentials.post(`/api/users/signup`, user);
+  return response.data;
+};
+
+export const signout = async () => {
+  const response = await axiosWithCredentials.post(`/api/users/signout`);
+  return response.data;
+};
+
+export const updateUser = async (user: any) => {
+  const response = await axiosWithCredentials.put(`/api/users/${user._id}`, user);
+  return response.data;
+};
